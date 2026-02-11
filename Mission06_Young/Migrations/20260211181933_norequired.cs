d0f258c59d39b936ef543340f96702455e5a5df8
peter <peter@LAPTOP17>
2026-02-11 11:23:59 -0700

    fixed database issues

---
+++ Mission06_Young/Migrations/20260211181933_norequired.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission06_Young.Migrations
+{
+    /// <inheritdoc />
+    public partial class norequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Movies",
+                type: "TEXT",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LentTo",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Edited",
+                table: "Movies",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Movies",
+                type: "TEXT",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LentTo",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Edited",
+                table: "Movies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+    }
+}
--- Mission06_Young/Models/MovieEntry.cs
+++ Mission06_Young/Models/MovieEntry.cs
@@ -17,10 +17,10 @@ namespace Mission06_Young.Models
         public string Director { get; set; }
         [Required]
         public string Rating { get; set; }
-        public bool Edited { get; set; }
-        public string LentTo { get; set; }
+        public bool? Edited { get; set; }
+        public string? LentTo { get; set; }
         [StringLength(25, ErrorMessage = "Notes cannot exceed 25 characters.")]
-        public string Notes { get; set; }
+        public string? Notes { get; set; }
 
     }
 }
