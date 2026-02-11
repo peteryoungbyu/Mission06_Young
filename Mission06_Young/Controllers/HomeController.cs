5777c2214fced43dda57912b90af163ed8157265
peter <peter@LAPTOP17>
2026-02-11 10:36:56 -0700

    Finish other pages

--- Mission06_Young/Controllers/HomeController.cs
+++ Mission06_Young/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mission06_Young.Models;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mission06_Young.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        private MovieEntryContext _context;
+
+        public HomeController(MovieEntryContext temp) //Constructor
+        {
+            _context = temp;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,9 +25,18 @@ namespace Mission06_Young.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Movies()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieEntry response)
+        {
+            _context.Movies.Add(response); // Add record to database
+            _context.SaveChanges();
+            return View("Confirmation", response);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
